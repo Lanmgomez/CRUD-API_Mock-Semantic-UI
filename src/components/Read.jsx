655756c0e7cb9f1f3ee0ae1669cd5887f4979cdb
islan <islan_gomes@hotmail.com>
2023-03-02 13:54:06 -0300

    Atualizações

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,15 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { Table } from 'semantic-ui-react'
+import { Table, Button } from 'semantic-ui-react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 const Read = () => {
 
+  // get api data
   const [APIData, setAPIData] = useState([]);
+
+  // get id for each data
   
   useEffect(() => {
     axios.get(`https://64009b6829deaba5cb3b893a.mockapi.io/CRUD`)
       .then((response) => { setAPIData(response.data) })
   }, [])
+
+  const setDataID = (data) => {
+    console.log(data);
+
+    let { id, firstName, lastName, checkbox } = data;
+      localStorage.setItem('ID', id);
+      localStorage.setItem('First Name', firstName);
+      localStorage.setItem('Last Name', lastName);
+      localStorage.setItem('Checkbox Value', checkbox)
+ }
 
   return (
     <div>
@@ -19,14 +33,20 @@ const Read = () => {
             <Table.HeaderCell>First Name</Table.HeaderCell>
             <Table.HeaderCell>Last Name</Table.HeaderCell>
             <Table.HeaderCell>Checked</Table.HeaderCell>
+            <Table.HeaderCell>Update</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {APIData.map((data) => (
-            <Table.Row>
+            <Table.Row key={data.id} id={data.id}>
               <Table.Cell>{data.firstName}</Table.Cell>
               <Table.Cell>{data.lastName}</Table.Cell>
               <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
+              <Table.Cell> 
+                <Link to='/update'>
+                  <Button onClick={() => setDataID(data)}>Update</Button>
+                </Link>
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
